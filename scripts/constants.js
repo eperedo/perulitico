a9ef38ae117853ccfcab1ae2ec2151339f7243b2
Eduardo P. Rivero <jrperedo@gmail.com>
2019-10-12 19:48:18 -0500

    feat(polls): Added total per type value

---
+++ scripts/constants.js
@@ -0,0 +1,58 @@
+const voteValues = {
+	SI: {
+		key: 'Si',
+		label: 'A favor',
+	},
+	NO: {
+		key: 'No',
+		label: 'En Contra',
+	},
+	ABST: {
+		key: 'Abst',
+		label: 'Abstencion',
+	},
+	AUS: {
+		key: 'Aus',
+		label: 'Ausente',
+	},
+	LE: {
+		key: 'Le',
+		label: 'Licencia por Enfermedad',
+	},
+	LO: {
+		key: 'Lo',
+		label: 'Licencia Oficial',
+	},
+	SINRESP: {
+		key: 'SinResp',
+		label: 'Sin Respuesta',
+	},
+	PC: {
+		key: 'Pc',
+		label: 'Presidente del Congreso',
+	},
+	F: {
+		key: 'Fallecidos',
+		label: 'Fallecidos',
+	},
+	SUS: {
+		key: 'Suspendidos',
+		label: 'Suspendidos',
+	},
+	JP: {
+		key: 'JuntaPortavoces',
+		label: 'Junta de Portavoces',
+	},
+	COM: {
+		key: 'ComisionOrdinaria',
+		label: 'Comision Ordinaria',
+	},
+	CEI: {
+		key: 'ComisionExtInternacional',
+		label: 'Junta de Portavoces',
+	},
+};
+
+module.exports = {
+	voteValues,
+};
